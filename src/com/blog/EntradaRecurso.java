952d50b05c7d9f5bd3da5211de47199be7aba460
alberto__segura <alberto.segura.delgado@gmail.com>
2015-12-03 20:21:35 +0100

    subida de fichero funciona

--- src/com/blog/EntradaRecurso.java
+++ src/com/blog/EntradaRecurso.java
@@ -2,13 +2,22 @@ package com.blog;
 
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import javax.websocket.server.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -17,9 +26,12 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 import javax.xml.bind.JAXBElement;
+
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
-
 
 
 @Path("/entrada")
@@ -99,6 +111,52 @@ public class EntradaRecurso {
 			return null;
 		}
 	}
+	
+	
+	// MuLTIPART para la parte multimedia
+	@POST
+	@Path("/nueva")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(@FormDataParam("archivo") InputStream uploadedInputStream, @FormDataParam("archivo") FormDataBodyPart fileDetail)//, @FormParam("id") String id, @FormParam("titulo") String titulo, @FormParam("contenido") String contenido)
+	{
+		System.out.println("LLEGA");
+		//System.out.println(titulo);
+		
+		String uploadedFileLocation = "./" + fileDetail.getName();
+
+		// save it
+		writeToFile(uploadedInputStream, uploadedFileLocation);
+
+		String output = "File uploaded to : " + uploadedFileLocation;
+
+		return Response.status(200).entity(output).build();
+
+	}
+	
+
+	// save uploaded file to new location
+	private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation) {
+
+		try {
+			OutputStream out = new FileOutputStream(new File(
+					uploadedFileLocation));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			out = new FileOutputStream(new File(uploadedFileLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+	
+	
 	
 	private Response putAndGetResponse(Entrada e)
 	{
